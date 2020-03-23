93bf87307d20a350c979d7ecd655d4e82179c97d
BARGHOUT <mohammed.barghout@atos.net>
2020-03-23 15:04:59 +0100

    firstCommit

--- projects/ten-thousand/src/ten-thousand/containers/add-player/add-player.component.ts
+++ projects/ten-thousand/src/ten-thousand/containers/add-player/add-player.component.ts
@@ -1,33 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {Router} from '@angular/router';
 import {PlayersService} from '../../services/players.service';
 
-
 @Component({
-  selector: 'add-player',
-  templateUrl: './add-player.component.html',
-  styleUrls: ['./add-player.component.css']
+    selector: 'add-player',
+    templateUrl: './add-player.component.html',
+    styleUrls: ['./add-player.component.css'],
 })
 export class AddPlayerComponent implements OnInit {
-    public myform: FormGroup;
+    public myform: FormGroup = new FormGroup({});
 
-  constructor(private fb : FormBuilder, private router: Router, private playerService: PlayersService) { }
+    constructor(
+        private fb: FormBuilder,
+        private router: Router,
+        private playerService: PlayersService,
+    ) {}
 
-  ngOnInit(): void {
-      this.createForm()
-  }
+    ngOnInit(): void {
+        this.createForm();
+    }
 
-  createForm(){
-     this.myform =  this.fb.group({
-          name: ''
-      })
-  }
+    createForm() {
+        this.myform = this.fb.group({
+            name: '',
+        });
+    }
 
     register() {
         // tslint:disable-next-line:no-console
-    console.log(this.myform.value)
-    this.playerService.register(this.myform.value)
-    this.router.navigate(['/ten-thousand', 'game'])
+        console.log(this.myform.value);
+        this.playerService.register(this.myform.value);
+        this.router.navigate(['/ten-thousand', 'game']);
     }
 }
--- projects/ten-thousand/src/ten-thousand/containers/launch-dices/launch-dices.component.ts
+++ projects/ten-thousand/src/ten-thousand/containers/launch-dices/launch-dices.component.ts
@@ -1,120 +1,114 @@
-
 import {Component, Input, OnInit} from '@angular/core';
 import {Player} from '../../interfaces/player';
 import {PlayersService} from '../../services/players.service';
 
 @Component({
-  selector: 'launch-dices',
-  templateUrl: './launch-dices.component.html',
-  styleUrls: ['./launch-dices.component.css']
+    selector: 'launch-dices',
+    templateUrl: './launch-dices.component.html',
+    styleUrls: ['./launch-dices.component.css'],
 })
 export class LaunchDicesComponent implements OnInit {
-  @Input() numbersDices = 5;
+    @Input() numbersDices = 5;
     public dices: number[] = [];
     public score = 0;
-    public dicesScore:  number[] = [] ;
+    public dicesScore: number[] = [];
     public sock = false;
-    public players: Player[];
-  constructor(private playerService : PlayersService) { }
+    public players: Player[] = [];
+    constructor(private playerService: PlayersService) {}
 
-  ngOnInit(): void {
-    this.players = this.playerService.players
-
-  }
-
+    ngOnInit(): void {
+        this.players = this.playerService.players;
+    }
 
     rollDices() {
-      if (this.sock){
-          this.sock = false;
-          alert('new player')
-          this.score = 0
-          this.dicesScore = []
-      }
+        if (this.sock) {
+            this.sock = false;
+            alert('new player');
+            this.score = 0;
+            this.dicesScore = [];
+        }
 
-    // reinitialise le tableau
-      this.dices = [];
+        // reinitialise le tableau
+        this.dices = [];
 
         // création du lancés aléatoire
-      for(let i = 0; i < this.numbersDices; i++){
-           this.dices.push( Math.ceil(Math.random()*6))
+        for (let i = 0; i < this.numbersDices; i++) {
+            this.dices.push(Math.ceil(Math.random() * 6));
         }
 
         // tslint:disable-next-line:no-console
-      console.log(this.dices)
+        console.log(this.dices);
 
-      this.analyzeDices()
+        this.analyzeDices();
     }
-    analyzeDices(){
+    analyzeDices() {
+        let score = false;
 
-    let score = false;
-
-
-    // on vérifie d'abord les suites
+        // on vérifie d'abord les suites
         // on vérifie la suite 1 2 3 4 5
-    if ( (this.dices.includes(1) && this.dices.includes(2) && this.dices.includes(3) && this.dices.includes(4) && this.dices.includes(5))
-        || (this.dices.includes(2) && this.dices.includes(3) && this.dices.includes(4) && this.dices.includes(5) && this.dices.includes(6) )
-        ){
-            this.score += 1500
-            this.dicesScore = this.dices
+        if (
+            (this.dices.includes(1) &&
+                this.dices.includes(2) &&
+                this.dices.includes(3) &&
+                this.dices.includes(4) &&
+                this.dices.includes(5)) ||
+            (this.dices.includes(2) &&
+                this.dices.includes(3) &&
+                this.dices.includes(4) &&
+                this.dices.includes(5) &&
+                this.dices.includes(6))
+        ) {
+            this.score += 1500;
+            this.dicesScore = this.dices;
             this.dices = [];
         }
 
-    const brelans = [
-        { val: 1 , value: 10 },
-        { val:2 , value: 2},
-        { val:3 , value: 3},
-        { val:4 , value: 4},
-        { val:5 , value: 5},
+        const brelans = [
+            {val: 1, value: 10},
+            {val: 2, value: 2},
+            {val: 3, value: 3},
+            {val: 4, value: 4},
+            {val: 5, value: 5},
+        ];
 
-    ];
+        // on calcul les brelans
+        brelans.forEach(combinaison => {
+            if (this.dices.filter(i => i === combinaison.val).length === 3) {
+                this.score += combinaison.value * 100;
+                this.numbersDices -= 3;
+                this.dicesScore = this.dices.filter(i => i === combinaison.val);
+                // on doit supprimer les dés qui on cette valeur
+                this.dices = this.dices.filter(i => i !== combinaison.val);
+            }
+        });
 
-    // on calcul les brelans
-    brelans.forEach(combinaison => {
-        if ( this.dices.filter(i => i === combinaison.val).length === 3){
-            this.score += combinaison.value * 100
-            this.numbersDices -= 3
-            this.dicesScore  = this.dices.filter(i => i === combinaison.val)
-            // on doit supprimer les dés qui on cette valeur
-            this.dices  = this.dices.filter(i => i !== combinaison.val)
+        this.dices.forEach((dice, index) => {
+            // on doit ici faire les calculs , 1 ou 5
 
+            if (dice === 1) {
+                this.score += 100;
+                this.clearDice(index);
+                score = true;
+            }
 
-        }
-    })
+            if (dice === 5) {
+                this.score += 50;
+                this.clearDice(index);
+                score = true;
+            }
+        });
 
-    this.dices.forEach( (dice, index) => {
-          // on doit ici faire les calculs , 1 ou 5
-
-          if ( dice === 1){
-              this.score += 100
-              this.clearDice(index)
-              score = true
-          }
-
-          if ( dice === 5){
-              this.score += 50
-              this.clearDice(index)
-              score = true
-          }
-
-
-      })
-
-    if (!score){
-        this.sock = true
-
-
+        if (!score) {
+            this.sock = true;
         }
     }
 
-
-    clearDice(index: number){
-
+    clearDice(index: number) {
         // sauvegarde du dé dans le compteur de score
-        this.dicesScore.push(this.dices[index])
+        this.dicesScore.push(this.dices[index]);
         // suppresion du dé
         delete this.dices[index];
         // réajustement du nombre de dé à lancé
-        this.numbersDices -= 1
-
+        this.numbersDices -= 1;
     }
 }
