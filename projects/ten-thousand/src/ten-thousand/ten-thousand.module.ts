c25a1ee3188736de272197e3a8acb45aaca98e41
BARGHOUT <mohammed.barghout@atos.net>
2020-03-23 17:51:52 +0100

    second commit

--- projects/ten-thousand/src/ten-thousand/ten-thousand.module.ts
+++ projects/ten-thousand/src/ten-thousand/ten-thousand.module.ts
@@ -1,18 +1,24 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import {CommonModule, registerLocaleData} from '@angular/common';
+import localeFrExtra from '@angular/common/locales/extra/fr';
+import localeFr from '@angular/common/locales/fr';
+import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import { DiceComponent } from './components/dice/dice.component';
-import { AddPlayerComponent } from './containers/add-player/add-player.component';
-import { LaunchDicesComponent } from './containers/launch-dices/launch-dices.component';
+import {DiceComponent} from './components/dice/dice.component';
+import {AddPlayerComponent} from './containers/add-player/add-player.component';
+import {LaunchDicesComponent} from './containers/launch-dices/launch-dices.component';
 import {TenThousandRoutingModule} from './ten-thousand-routing.module';
 
+registerLocaleData(localeFr, 'fr-FR', localeFrExtra);
 
 @NgModule({
-  declarations: [DiceComponent, LaunchDicesComponent, AddPlayerComponent],
-  imports: [
-    CommonModule, FontAwesomeModule, TenThousandRoutingModule, ReactiveFormsModule
-  ],
-    exports: [ LaunchDicesComponent],
+    declarations: [DiceComponent, LaunchDicesComponent, AddPlayerComponent],
+    imports: [
+        CommonModule,
+        FontAwesomeModule,
+        TenThousandRoutingModule,
+        ReactiveFormsModule,
+    ],
+    exports: [LaunchDicesComponent],
 })
-export class TenThousandModule { }
+export class TenThousandModule {}
