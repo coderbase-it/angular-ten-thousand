5515fb4d8a23cb6d0a65717a5ad044889c0f5719
pierre <pierrenedelec46@gmail.com>
2020-03-24 11:46:49 +0100

    update

--- projects/demo/src/app/app.browser.module.ts
+++ projects/demo/src/app/app.browser.module.ts
@@ -4,16 +4,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app.routes';
 import {StaticModule} from './modules/static/static.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
     bootstrap: [AppComponent],
     imports: [
-        BrowserModule.withServerTransition({appId: 'demo'}),
+        BrowserModule.withServerTransition({appId: 'demo-superman'}),
         AppRoutingModule,
         StaticModule,
         FontAwesomeModule,
-
     ],
     declarations: [AppComponent],
     providers: [
--- projects/demo/src/app/modules/lazy/lazy.module.ts
+++ projects/demo/src/app/modules/lazy/lazy.module.ts
@@ -1,19 +1,24 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LazyComponent} from './lazy.component';
+import {LazyChildComponent} from './lazy-child/lazy-child.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: LazyComponent,
+        children: [
+            {
+                path: 'child/:id',
+                component: LazyChildComponent,
+            },
+        ],
     },
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forChild(routes),
-    ],
-    declarations: [LazyComponent],
+    imports: [RouterModule.forChild(routes)],
+    declarations: [LazyComponent, LazyChildComponent],
     exports: [LazyComponent],
 })
 export class LazyModule {}
