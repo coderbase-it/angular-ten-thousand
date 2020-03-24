36b0fc58164d7bb1aa2720b7d39a6a447c3e7256
Etienne VIVIEN <vivien.etienne@gmail.com>
2020-03-24 15:18:07 +0100

    feat(players): Add current player to service

--- projects/ten-thousand/src/ten-thousand/containers/launch-dices/launch-dices.component.ts
+++ projects/ten-thousand/src/ten-thousand/containers/launch-dices/launch-dices.component.ts
@@ -9,24 +9,25 @@ import {PlayersService} from '../../services/players.service';
     templateUrl: './launch-dices.component.html',
     styleUrls: ['./launch-dices.component.css'],
 })
-export class LaunchDicesComponent implements OnInit {
-    public dices$: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
-    public score$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
-    public dicesScore: number[] = [];
-    public sock$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    public players: IPlayer[] = [];
+export class LaunchDicesComponent {
+    public dices$: BehaviorSubject<number[]>;
+    public score$: BehaviorSubject<number>;
+    public dicesScore: number[];
+    public sock$: BehaviorSubject<boolean>;
+    public players: IPlayer[];
+    public currentPlayer$: BehaviorSubject<IPlayer>;
     constructor(
         private playerService: PlayersService,
         private calculService: CalculService,
-    ) {}
-
-    ngOnInit(): void {
+    ) {
         this.players = this.playerService.players;
         this.dices$ = this.calculService.dices$;
         this.score$ = this.calculService.score$;
         this.dicesScore = this.calculService.dicesScore;
         this.sock$ = this.calculService.sock$;
+        this.currentPlayer$ = this.playerService.currentPlayer$;
     }
+
     rollDices() {
         this.calculService.rollDices();
     }
--- projects/ten-thousand/src/ten-thousand/services/players.service.ts
+++ projects/ten-thousand/src/ten-thousand/services/players.service.ts
@@ -1,13 +1,20 @@
 import {Injectable} from '@angular/core';
 import {IPlayer} from '../interfaces/player';
 import {StockageService} from '../services/stockage.service';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class PlayersService {
+    public currentPlayer$: BehaviorSubject<IPlayer> = new BehaviorSubject<IPlayer>({
+        name: '',
+        score: 0,
+        paliers: [],
+    });
     public players: IPlayer[] = [];
 
+    private currentIndex = 0;
     constructor(private _stockage: StockageService) {}
 
     register(playerNames: string[]) {
@@ -17,5 +24,11 @@ export class PlayersService {
 
         this.players = players;
         this._stockage.sauvegardeDeLaPartie(this.players);
+        this.nextPlayer();
+    }
+
+    nextPlayer() {
+        this.currentPlayer$.next(this.players[this.currentIndex]);
+        this.currentIndex = this.currentIndex + (1 % this.players.length);
     }
 }
