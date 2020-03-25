376d2a37b4f6dca4860e47d6477707fb47c0e4be
Etienne VIVIEN <vivien.etienne@gmail.com>
2020-03-25 10:46:04 +0100

    feat(change-detection): Add OnPush detection strategy to AddPlayer

--- projects/ten-thousand/src/ten-thousand/containers/add-player/add-player.component.ts
+++ projects/ten-thousand/src/ten-thousand/containers/add-player/add-player.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
 import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {PlayersService} from '../../services/players.service';
@@ -7,6 +7,7 @@ import {PlayersService} from '../../services/players.service';
     selector: 'add-player',
     templateUrl: './add-player.component.html',
     styleUrls: ['./add-player.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddPlayerComponent implements OnInit {
     public myform: FormGroup = new FormGroup({});
@@ -45,10 +46,16 @@ export class AddPlayerComponent implements OnInit {
             this.fb.control('', [Validators.required, Validators.minLength(1)]),
         );
     }
+
     removePlayer(index: number) {
         this.playersArray.removeAt(index);
     }
+
     canDeactivate(): boolean {
         return this.myform.dirty && !this.myform.pristine;
+    }
+
+    check() {
+        console.log('check detection on addPlayer');
     }
 }
